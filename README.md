717253681a884facfa5cf04708e7b6d82d9fd4ec
happylie <happylie@hanmail.net>
2022-06-05 14:26:06 +0900

    Upload

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
-# crk
-Cookie Run Kingdom Coupon Insert
+# CookieRunKingdom-Coupon
+<div>
+<img src="https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Fgithub.com%2Fhappylie%2FCookieRunKingdom-Coupon&count_bg=%2379C83D&title_bg=%23555555&icon=github.svg&icon_color=%23E7E7E7&title=view&edge_flat=false"/>
+<img src="https://img.shields.io/badge/Python->=3.5-blue?logo=python&logoColor=white" />
+</div>
+
+쿠키런 킹덤 쿠폰 손쉬운 입력 Tool
+- https://happylie.tistory.com/
+
+## 쿠키런 킹덤 쿠폰 입력 Tool
+### 설치 방법
+1. Git Clone
+```
+$ https://github.com/happylie/CookieRunKingdom-Coupon.git
+```
+2. Requirements 설치
+```
+$ pip install -r requirements.txt
+```
+### 실행 방법
+1. Help
+```
+$ python run.py -h                 
+usage: Cookie Run Kingdom Coupon [-h] [-c COUPON_CODE] [-v]
+
+쿠키런 킹덤 쿠폰 등록
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -c COUPON_CODE, --coupon COUPON_CODE
+                        쿠키런 킹덤 쿠폰 번호
+  -v, --version         show program's version number and exit
+```
+2. 쿠폰 등록 유저 등록
+```
+run.py 파일에서 15번째줄에 있는 user_data = ['GUEST-XXXXXXXX', 'aaa@aaa.com'] 과 같이 본인 유저 정보를 입력 한다.  
+```
+3. 쿠폰 등록
+```
+$ python run.py -c DEVNOW40MKINGDOM
+GUEST-XXXXXXXX :: 상품이 정상적으로 지급되었습니다.
+aaa@aaa.com :: DevPlay 계정을 다시 한번 확인해주세요.
+```
---
+++ run.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import sys
+import json
+import requests
+import argparse
+
+
+class CookieRunKingdom:
+    def __init__(self):
+        self.ua = "Mozilla/5.0(Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.63 Safari/537.36"
+
+    @staticmethod
+    def __user_data():
+        user_data = ['GUEST-YKPRX54058', 'GUEST-WVHPD57513']
+        return user_data
+
+    @staticmethod
+    def __get_result(ret):
+        if ret == 20000:
+            return "상품이 정상적으로 지급되었습니다."
+        elif ret == 40006:
+            return "DevPlay 계정을 다시 한번 확인해주세요."
+        elif ret == 42501:
+            return "사용 기간이 만료된 쿠폰입니다."
+        elif ret == 42502:
+            return "쿠폰번호를 다시 한번 확인해주세요."
+        elif ret == 42201 or ret == 42503:
+            return "해당 계정으로 이미 같은 종류의 쿠폰을 등록하셨습니다."
+        elif ret == 42203 or ret == 42504:
+            return "이미 사용된 쿠폰입니다."
+        else:
+            return "서버에서 알 수 없는 응답이 발생하였습니다. 잠시후 다시 시도해주세요."
+
+    def __post_coupon(self, post_data):
+        url = 'https://account.devplay.com/v2/coupon/ck'
+        headers = {'Content-Type': 'application/json', 'User-Agent': self.ua}
+        try:
+            r = requests.post(url=url, headers=headers, json=post_data)
+            if r.status_code != 200:
+                return self.__get_result(1)
+            else:
+                obj = json.loads(r.text)
+                return self.__get_result(obj.get('code'))
+        except Exception as err:
+            print("post_coupon:: {err}".format(err=err))
+
+    def registration_coupon(self, coupon_data):
+        for user in self.__user_data():
+            post_data = {
+                "email": user,
+                "coupon_code": coupon_data
+            }
+            print("{user} :: {result}".format(user=user, result=self.__post_coupon(post_data)))
+        return True
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(prog='Cookie Run Kingdom Coupon', description='쿠키런 킹덤 쿠폰 등록')
+    try:
+        parser.add_argument('-c', '--coupon', dest='coupon_code', type=str, help='쿠키런 킹덤 쿠폰 번호')
+        parser.add_argument('-v', '--version', action='version', version='%(prog)s 1.0')
+        args = parser.parse_args()
+        crk = CookieRunKingdom()
+        if args:
+            crk.registration_coupon(args.coupon_code)
+            sys.exit(0)
+    except Exception as err:
+        print("main:: {err}".format(err=err))
+        sys.exit(0)
